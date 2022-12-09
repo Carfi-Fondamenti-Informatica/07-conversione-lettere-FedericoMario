0720b1baec9680c391f5bb4ed5b83c73aaf37ba0
FedericoMario <117830052+FedericoMario@users.noreply.github.com>
2022-12-09 08:20:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-#include "lib.h"
 
-using namespace std;
-int main() {
-    char a;
-    cin >>a;
-    if (convert(a)){
-        cout<<a<<endl;
-    }else{
-        cout<<"errore"<<endl;
-    }
-    return 0;
-
-  return 0;
-}
